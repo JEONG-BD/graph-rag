4f4ab0755a43f1f28cf20e29e6845aa335dea3cf
JEONG-BD <dev.byeongdu@gmail.com>
2026-08-07 15:02:04 +0900

    llm client

--- common/llm_client.py
+++ common/llm_client.py
@@ -6,30 +6,31 @@ from langsmith import traceable
 
 settings = get_settings()
 
-class LLMClient:
 
+class LLMClient:
     def __init__(
-            self,
-            model: str =  "google/gemma-4-e4b",
-            temperature: float = 0.5,
+        self,
+        model: str = "google/gemma-4-e4b",
+        temperature: float = 0.5,
     ) -> None:
-        self._llm  =  ChatOpenAI(
-            model = model or settings.default_model,
-            base_url = settings.base_url,
-            api_key = settings.api_key,
-            temperature = temperature
+        self._llm = ChatOpenAI(
+            model=model or settings.default_model,
+            base_url=settings.base_url,
+            api_key=settings.api_key,
+            temperature=temperature,
         )
+
     @traceable(
         name="LLMClient.chat",
         run_type="chain",
     )
-    def chat(self, messages: list[dict[str, Any]]) -> str:
+    def invoke(self, messages: list[dict[str, Any]]) -> str:
         response: AIMessage = self._llm.invoke(
             messages,
             config={
                 "run_name": "chat-model-invoke",
                 "metadata": {
-                    "model_name": self._model_name,
+                    "model_name": self.get_model_name,
                     "client": "LLMClient",
                 },
                 "tags": ["chat", "llm-client"],
@@ -40,6 +41,3 @@ class LLMClient:
 
     def get_model_name(self) -> str:
         return self._llm.model_name
-
-
-
