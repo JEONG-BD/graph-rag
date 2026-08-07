478db1e7bdc708ac20140474467c79cbb1882fad
JEONG-BD <dev.byeongdu@gmail.com>
2026-08-07 15:02:40 +0900

    embedding client

--- common/embedding_client.py
+++ common/embedding_client.py
@@ -1,28 +1,34 @@
 from langchain_openai import OpenAIEmbeddings
-
+from transformers import AutoTokenizer
 from settings import get_settings
 
 settings = get_settings()
 
-class EmbeddingClient():
 
-    def __init__(self, model: str = ""):
+class EmbeddingClient:
+    def __init__(self, model: str = "", pretrained_model: str = ""):
         self._embedding = OpenAIEmbeddings(
-            model= model or settings.embedding_model,
+            model=model or settings.embedding_model,
             base_url=settings.base_url,
             api_key=settings.api_key,
-            check_embedding_ctx_length=False
+            check_embedding_ctx_length=False,
+        )
+        self._tokenizer = AutoTokenizer.from_pretrained(
+            pretrained_model or settings.pretrained_model
         )
 
-    def get_size(self, text:str) -> int:
+    def get_size(self, text: str) -> int:
         return len(self.embed_text(text))
         return len(self._embedding.embed_query(text))
 
-    def embed_query(self, text:str)-> list:
+    def embed_query(self, text: str) -> list:
         return self._embedding.embed_query(text)
 
-    def embed_documents(self, text:list[str])-> list[float]:
+    def embed_documents(self, text: list[str]) -> list[float]:
         return self._embedding.embed_documents(text)
 
     def get_model_name(self) -> str:
         return self._embedding.model
+
+    def num_tokens(self, text: str) -> int:
+        return len(self._tokenizer.encode(text))
